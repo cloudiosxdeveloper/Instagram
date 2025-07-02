6a08edc18f9e6afe46e6f51a4f2a1e5ff1b4cc31
John Kim <jarvis@Mac.lan>
2025-07-02 17:29:43 -0400

    Initialize git and set-up tab bar

---
+++ Instagram/App/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by John Kim on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
